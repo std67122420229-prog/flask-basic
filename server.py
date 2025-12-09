a0796bc8fcfc56603d306e9411c9fe87619aa276
piyaporn <std.67122420229@ubru.ac.th>
2025-12-09 13:58:25 +0700

    เพิ่ม  route

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import uuid
 
 app = Flask(__name__)
 
@@ -26,3 +27,33 @@ def addition(num1, num2):
   return f'''
   {num1} + {num2} = {result}  
   ''' 
+@app.route('/user/<username>')
+def user(username):
+  return f'<h1> name is: {username} </h1>'
+
+@app.route('/calculator/addition/<int:num1>/<int:num2>')
+def addition(a, b):
+  result = a + b
+  return f'<h1> {a} + {b} = {a+b} </h1>'
+
+@app.route('/calculator/division/<int:a>/<int:b>')
+def division(a, b):
+  result = a - b
+  return f'<h1> {a} / {b} = {a-b} </h1>'
+
+@app.route('/calculator/multiplication/<int:a>/<int:b>')
+def multiplication(a, b):
+  result = a * b
+  return f'<h1> {a} * {b} = {result} </h1>'
+
+@app.route('/calculator/subtraction/<int:a>/<int:b>')
+def subtraction(a, b):
+  result = a / b
+  return f'<h1> {a} / {b} = {result} </h1>'
+
+@app.route('/secretkey/<uuid:sk')
+def My_secretkey(sk):
+  return f'<h1> My secret key is: {sk} </h1>'
+
+#if __name__ == '__main__': 
+#  app.run(debug=True)
